073473664ab21e6eb6197309536599a4129bf2ab
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-05-17 11:05:12 +0500

    about page

--- src/Router.tsx
+++ src/Router.tsx
@@ -29,7 +29,7 @@ export const routes = createBrowserRouter([
             element: <AboutUz />,
           },
           {
-            path: "about-us",
+            path: "about",
             element: <About_us />,
           },
           {
